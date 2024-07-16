1a90e374c6192e4f19b0c459d4573cf35564966a
airlorm <elormnizi@gmail.com>
2024-07-16 11:36:01 +0300

    Add the delete function for project

--- src/component/project/SelectedProject.jsx
+++ src/component/project/SelectedProject.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "../inputs/Button";
 
-export default function SelectedProject({ project }) {
+export default function SelectedProject({ project, onDelete }) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
@@ -12,7 +12,7 @@ export default function SelectedProject({ project }) {
       <header className="pb-4 mb-4 border-b-2 border-stone-300">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-stone-600">{project.title}</h1>
-          <Button>Delete</Button>
+          <Button onClick={onDelete}>Delete</Button>
         </div>
         <p className="mb-4 text-stone-400">{formattedDate}</p>
         <p className="text-stone-600 whitespace-pre-wrap">
